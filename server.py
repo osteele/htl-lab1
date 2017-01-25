bfcadde939950a75118c83ba750b524b71ca63a2
Oliver Steele <steele@osteele.com>
2017-01-25 08:22:13 -0500

    try turning debug back on

--- server.py
+++ server.py
@@ -31,4 +31,4 @@ def area_page(course_area):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', debug=DEBUG, port=port)
+    app.run(host='0.0.0.0', debug=True, port=port)
